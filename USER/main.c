4ed7501400af44df95fed958ba5299efc9358a1c
moujiawang <1254734965@qq.com>
2018-12-20 10:38:21 +0800

    Modify:The abnormal instruction value is judged. If the instruction value is not within the normal range, it is discarded.

--- USER/main.c
+++ USER/main.c
@@ -14,6 +14,7 @@ volatile uint8_t Channel_Num = 0;
 volatile uint8_t Update_Num = 0;
 volatile uint8_t Channel_status = End;
 volatile uint8_t Receive_complete_flag = 0;
+volatile uint8_t Receive_Wrong_flag = 0;
 volatile uint8_t Do_Flag = 0;											//while loop time control flag，when Do_flag = 0，while loop can execute
 
 IMUFusion imu_fusion_module;
@@ -63,6 +64,8 @@ void TIM4_IRQHandler()
 		if( Channel_status != End )
 		{
 			Receive_length[Channel_Num] = New_count + ((uint32_t)Update_Num * TIM4_PERIOD) - Last_count;
+			if((Receive_length[Channel_Num] <= 8000) || (Receive_length[Channel_Num] >= 16000))
+				Receive_Wrong_flag = 1;
 			Channel_Num++;
 		}
 		else
@@ -79,13 +82,14 @@ void TIM4_IRQHandler()
 		Update_Num++;
 		if( Update_Num > 3 )										//接收到的通道指令最大值为大于16000，小于17000，所以TIM4在通道指令没发完前最多可能溢出3次
 		{
-			if( Channel_Num == 10 )									//接受完9条通道的指令
+			if((Channel_Num == 10) && (Receive_Wrong_flag == 0))	//接受完9条通道的指令，且接收到的指令中没有异常的指令（小于8000（最小指令值）和大于16000（最大指令值））
 			{
 				Receive_complete_flag = 1;							//指令接受完成标志位置位
 			}
 			else													//如果连续两次计数器溢出还没有一次的捕获，说明在两条指令之间，但是如果指令数不等于10说明前面的数据有通道的指令被丢弃或者误识通道指令，所以后面程序将收到的通道指令全部丢弃；
 			{
 				memset(&Receive_length, 0, sizeof(Receive_length));	//将Receive_Length数组清零
+				Receive_Wrong_flag = 0;
 			}
 			Channel_status = End;									//指令接收完成标志位没有置位，不会更新PWM占空比
 		}
